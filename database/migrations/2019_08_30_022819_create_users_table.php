79f106235102287024cd4105787aa5c03e2ee389
Marc Enzo <emarc237@gmail.com>
2019-08-30 03:34:15 +0100

    Initial commit

---
+++ database/migrations/2019_08_30_022819_create_users_table.php
@@ -0,0 +1,39 @@
+<?php
+	
+	use Illuminate\Support\Facades\Schema;
+	use Illuminate\Database\Schema\Blueprint;
+	use Illuminate\Database\Migrations\Migration;
+	
+	class CreateUsersTable extends Migration
+	{
+		/**
+		 * Run the migrations.
+		 *
+		 * @return void
+		 */
+		public function up()
+		{
+			Schema::create('users', function (Blueprint $table) {
+				$table->bigIncrements('id');
+				$table->string('first_name');
+				$table->string('last_name')->nullable();
+				$table->timestamp('date_of_birth');
+				$table->string('birthplace');
+				$table->char('gender');
+				$table->string('phone')->unique();
+				$table->string('password');
+				$table->integer('type')->default(0);
+				$table->timestamps();
+			});
+		}
+		
+		/**
+		 * Reverse the migrations.
+		 *
+		 * @return void
+		 */
+		public function down()
+		{
+			Schema::dropIfExists('users');
+		}
+	}
